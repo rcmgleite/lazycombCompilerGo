caf974bab808d788adfc49c8362f1225a96d24ab
Rafael Leite <r.cmgleite@gmail.com>
2015-12-04 01:08:15 -0200

    verifying exit status from lazycompiler before continuing

--- compileAndExecute.sh
+++ compileAndExecute.sh
@@ -1,6 +1,9 @@
 echo $1
 go run main.go lex.go syntatic.go semantic.go $1
-echo ''
-echo ''
-echo '[INFO] Initializing out.c compilation using GCC'
-make run
+if [[ $? == 1 ]]
+then
+	echo '[INFO] exiting...'
+else
+	echo '[INFO] Initializing out.c compilation using GCC'
+	make run
+fi
